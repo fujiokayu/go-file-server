9ecee27f00cccb0f7fd7ba8f6b0b5e8067ed5791
fujiokayu <fujiokayu@gmail.com>
2019-04-17 21:36:16 +0900

    Modify commit c2e09c1, and clean up code.

--- src/main.go
+++ src/main.go
@@ -10,14 +10,9 @@ import (
 
 func htmlHandler(w http.ResponseWriter, r *http.Request) {
 
-	http.StripPrefix("/", http.FileServer(http.Dir("./static")))
+	http.StripPrefix("/", http.FileServer(http.Dir("/static/")))
 	t := template.Must(template.ParseFiles("static/index.html"))
 
-	/*	m := map[string]string{
-			"title": "golang file server",
-			"text":  "this page will be a simple file server",
-		}
-	*/
 	files, _ := ioutil.ReadDir("./contents")
 	for _, f := range files {
 		fmt.Println(f.Name())
